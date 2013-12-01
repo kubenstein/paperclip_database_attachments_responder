35b033876cfdfcbd80fd2bc489cb189d77c4a67c
Jakub Niewczas <niewczas.jakub@gmail.com>
2013-12-01 23:36:18 +0100

    use instead of insert_after for rails 3 compatibility

--- lib/paperclip_database_attachments_responder/railtie.rb
+++ lib/paperclip_database_attachments_responder/railtie.rb
@@ -2,7 +2,7 @@ module PaperclipDatabaseAttachmentsResponder
 
   class Railtie < Rails::Railtie
     initializer "paperclip_database_attachments_responder.configure_rails_initialization" do |app|
-      app.middleware.insert_after 'Rack::Head', 'PaperclipDatabaseAttachmentsResponder::Middleware'
+      app.middleware.use 'PaperclipDatabaseAttachmentsResponder::Middleware'
     end
   end
 
